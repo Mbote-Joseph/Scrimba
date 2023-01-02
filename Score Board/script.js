331ab0a7980c86a19ee3dc3abb0454f2f95bd1fa
Mbote-Joseph <mbotejoseph001@gmail.com>
2023-01-02 17:10:32 +0300

    Scrimba Courses Learning

---
+++ Score Board/script.js
@@ -0,0 +1,47 @@
+var scorehome = 0;
+var scoreguest = 0;
+var scorehome1 = document.getElementById("scorehome");
+var scoreguest1 = document.getElementById("scoreguest");
+
+function reset() {
+  scorehome = 0;
+  scoreguest = 0;
+  scorehome1.innerHTML = scorehome;
+  scoreguest1.innerHTML = scoreguest;
+}
+
+function addHome() {
+  scorehome++;
+  scorehome1.innerHTML = scorehome;
+  console.log(scorehome);
+}
+
+function addGuest() {
+  scoreguest++;
+  scoreguest1.innerHTML = scoreguest;
+  console.log(scoreguest);
+}
+
+function addHome2() {
+  scorehome + 2;
+  scorehome1.innerHTML = scorehome;
+  console.log(scorehome);
+}
+
+function addGuest2() {
+  scoreguest + 2;
+  scoreguest1.innerHTML = scoreguest;
+  console.log(scoreguest);
+}
+
+function addHome3() {
+  scorehome + 3;
+  scorehome1.innerHTML = scorehome;
+  console.log(scorehome);
+}
+
+function addGuest3() {
+  scoreguest + 3;
+  scoreguest1.innerHTML = scoreguest;
+  console.log(scoreguest);
+}
